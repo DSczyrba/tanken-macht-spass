45e7201bafea8c5da01e69f1a6f9aefa892b6ffb
Chicken69 <dominic.sczyrba@gmail.com>
2022-02-09 13:55:13 +0000

    yo

--- get_id.py
+++ get_id.py
@@ -1,19 +1,27 @@
 import requests, os, json
 
-data = requests.get(
-    "https://creativecommons.tankerkoenig.de/json/list.php",
-    params={
-        "apikey": os.environ.get("TANK_API"),
-        "lat": "50.7278999",
-        "lng": "12.9094443",
-        "rad": "5",
-        "type": "all"
-    }
-).json()
 
-if data['status'] != "ok":
-  print("Daten konnten nicht geladen werden...")
-  exit()
+def get_ids():
+  data = requests.get(
+      "https://creativecommons.tankerkoenig.de/json/list.php",
+      params={
+          "apikey": os.environ.get("TANK_API"),
+          "lat": "50.7278999",
+          "lng": "12.9094443",
+          "rad": "50",
+          "type": "all"
+      }
+  ).json()
 
-for x in data['stations']:
-  print(f"Name: {x['name']} ID: {x['id']}")
+  if data['status'] != "ok":
+    print("Daten konnten nicht geladen werden...")
+    exit()
+
+
+  ids = []
+
+  #ids = [x["id"] for x in data["stations"]]
+  for x in data['stations']:
+    print(f"Name: {x['name']} ID: {x['id']}")
+    ids.append(x['id'])
+  return ids
--- get_prices.py
+++ get_prices.py
@@ -1,10 +1,12 @@
 import requests, os, json
+from get_id import get_ids
+
 
 data = requests.get(
     "https://creativecommons.tankerkoenig.de/json/prices.php",
     params={
         "apikey": os.environ.get("TANK_API"),
-        "ids": "0e4638dc-c2e5-41b8-b6dd-e5f38df5ea95,a5141b3c-9d0d-443b-9d14-feb78ba8c4a9"
+        "ids": ",".join(get_ids())
 
     }
 ).json()
@@ -15,4 +17,5 @@ if data['ok'] is False:
   exit()
 
 
-print(data)
+for x in data['prices']:
+  print(data['prices'][x])
